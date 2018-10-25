6530454666a7b688b5450b3b1261851d0ff3e275
Francisco Machado <francisco.machado@academiadecodigo.org>
2018-10-25 21:53:20 +0100

    correct head title

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default class extends React.Component {
     render() {
         return (
             <Head>
-                <title>&lt;A/C_&bt; Randomizer</title>
+                <title>&lt;A/C_&gt; Randomizer</title>
                 <link href="/static/styles.css" rel="stylesheet" />
             </Head>
         )
