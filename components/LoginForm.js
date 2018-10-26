3310521888c0e3980dae5ace4afa2944e77ee253
Francisco Machado <francisco.machado@academiadecodigo.org>
2018-10-26 16:25:14 +0100

    add styles to login form

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,45 +1,71 @@
 import Flex from '../components/Flex';
 import Card from '../components/Card';
+import styled from 'styled-components';
+
+const Form = styled.form`
+    margin: 10px auto;
+    background: #fff;
+    padding: 10px;
+    border-radius: 5px;
+    text-align: center;
+    color: black;
+    width: 200px;
+`;
+
+const Label = styled.label`
+    text-align: center;
+    display: block;
+    margin: 10px auto;
+`;
+
+const Input = styled.input`
+    margin: auto 10px;
+`;
+
+const Submit = styled.input`
+    padding: 10px;
+    text-align: center;
+    background: #e31d1a;
+    color: #fff;
+    border: none;
+    text-transform: uppercase;
+    outline: none;
+`;
 
 export default class extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {username: '', password: ''};
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     render() {
         return (
             <Flex direction={Flex.DIRECTION.HORIZONTAL} style={{ height: '50vh' }}>
-                <form onSubmit={this.handleSubmit}>
-                    <Card>
-                        <label>
+                <Form onSubmit={this.handleSubmit}>
+                        <Label>
                             Username:
-                            <input 
+                            <Input 
                                 type="text"
                                 name="username"
                                 value={this.state.username}
                                 onChange={this.handleInputChange}/>
-                        </label>
-                        <label>
+                        </Label>
+                        <Label>
                             Password:
-                            <input 
+                            <Input 
                                 type="password" 
                                 name="password" 
                                 value={this.state.password} 
                                 onChange={this.handleInputChange}/>
-                        </label>
-                        <input type="submit"/>
-                    </Card>
-                    <label>{this.props.error}</label>
-                </form>
+                        </Label>
+                        <Submit type="submit"/>
+                        <Label style={{ color: 'red', fontSize: '10px' }}>{this.props.error}</Label>
+                </Form>
             </Flex>
         );
     }
 
-    handleInputChange(event) {
+    handleInputChange = (event) => {
         const target = event.target;
         const value = target.value;
         const name = target.name;
@@ -49,7 +75,7 @@ export default class extends React.Component {
         });
     }
 
-    handleSubmit(event) {
+    handleSubmit = (event) => {
         event.preventDefault();
         this.props.handleSubmit(this.state.username, this.state.password);
     }
