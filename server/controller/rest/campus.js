1dfad0f314bd2a2a82d5cb296c68d4cdb7ad462b
Francisco Machado <francisco.machado@academiadecodigo.org>
2018-10-27 12:39:03 +0100

    add validations to rest campus controller

--- server/controller/rest/campus.js
+++ server/controller/rest/campus.js
@@ -24,6 +24,10 @@ module.exports = {
             const campus = new Campus(req.body);
             
             campus.save((err, c) => {
+                if (err) {
+                    return res.status(500).json({error: 'error creating campus'})
+                }
+
                 res.status(201).json({
                     path: `/campus/${c.location}`
                 });
@@ -44,22 +48,37 @@ module.exports = {
         // create cadet related to campus
         server.post(`${CAMPUS_API_ROOT}/:location/cadet`, tokenManager.validate, (req, res) => {
             Campus.findOne({location: ignoreCase(req.params.location)}, (err, campus) => {
+                if (!campus) {
+                    return res.status(404).json({error: 'invalid location'});
+                }
+
                 const cadet = new Cadet(req.body);
                 campus.cadets.push(cadet);
 
-                cadet.save(() => {
-                    campus.save((err, updatedModel) => {
-                        res.status(201).send();
-                    });
+                campus.save((err) => {
+                    if (err) {
+                        return res.status(500).json({error: 'error persisting changes'})
+                    }
+
+                    res.status(201).send();
                 });
             });
         });
 
+        // update campus cadet list
         server.put(`${CAMPUS_API_ROOT}/:location/cadet`, tokenManager.validate, (req, res) => {
+            if (!req.body.cadets) {
+                return res.status(400).json({error: 'invalid request'});
+            }
+
             Campus.findOne({location: ignoreCase(req.params.location)}, (err, campus) => {
+                if (!campus) {
+                    return res.status(404).json({error: 'invalid location'});
+                }
+
                 campus.cadets = req.body.cadets;
 
-                campus.save((err, updatedModel) => {
+                campus.save(() => {
                     res.status(201).send();
                 });
             });
